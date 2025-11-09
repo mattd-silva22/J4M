7ade32c13385885a74b18c16aebdf5c7c3706715
JoaoPCP <joaopcp23@gmail.com>
2025-11-08 23:18:59 -0300

    refactor: adaptando para o espaçamento pre pronto no Layout geral

--- src/pages/homePage/components/Community.jsx
+++ src/pages/homePage/components/Community.jsx
@@ -5,7 +5,7 @@ const arr = Array.apply(null, Array(24)).map(function () {});
 function Community() {
   console.log(arr);
   return (
-    <div className="flex flex-col gap-10 px-20">
+    <div className="flex flex-col gap-10">
       <h1 className="text-pink font-orbitron text-3xl">COMUNIDADE</h1>
       <div className="grid grid-cols-8 justify-center items-center gap-5">
         {arr.map(() => (
--- src/pages/homePage/components/Oficials.jsx
+++ src/pages/homePage/components/Oficials.jsx
@@ -2,7 +2,7 @@ import CardOficialJam from "../../../components/CardOficialJam";
 
 function Oficials() {
   return (
-    <section className="flex flex-col gap-13 px-20 py-15">
+    <section className="flex flex-col gap-13">
       <h2 className="font-orbitron text-3xl text-pink">OFICIAIS</h2>
       <div className="flex gap-5 justify-center items-center">
         <CardOficialJam size="slim" color="green" />
